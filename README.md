d56caceb4a634139cf567eccd34bef7cfd6e6a38
jerdeb <jerdebattista@gmail.com>
2014-11-20 13:35:49 +0100

    updated first version

--- README.md
+++ README.md
@@ -2,3 +2,19 @@
 ===========
 
 A small and simple dataset crawler on 270a.info for Quality Assessment
+
+Pre-requisits
+------------
+	- RDFLib
+	- RDFLib-JSONLD
+	- pyparsing v. 1.5.7
+
+
+RDFLib : easy_install rdflib <<Requires SUDO rights>>
+
+PyParsing : easy_install http://cheeseshop.python.org/packages/source/p/pyparsing/pyparsing-1.5.7.tar.gz  <<Requires SUDO rights>>
+
+RDFLib-JSONLD
+	(a) git clone https://github.com/RDFLib/rdflib-jsonld.git
+	(b) python setup.py build
+	(c) python setup.py install <<Requires SUDO rights>>
--- crawler.py
+++ crawler.py
@@ -5,9 +5,12 @@ import ntpath
 import os
 import tarfile
 import requests
+import rdfextras
 
 from os import listdir
 from os.path import isfile, join
+from rdflib.serializer import Serializer
+from rdflib import plugin
 
 # Functions
 def identifySerialisation( fileName ):
@@ -17,10 +20,20 @@ def identifySerialisation( fileName ):
     else:
         return
         
+def loadMetricConfiguration():    
+    g = rdflib.Graph();
+    config = g.parse("config.ttl", format="turtle")
+    return g.serialize(format="json-ld", indent=0)
+    
+def formatMetricConfiguration(configStr):
+    formattedStr = configStr.replace('\n', ' ').replace('\r', '').replace('"','\"')
+    return formattedStr
 
 # Main
 with open('voidlist.csv', 'rb') as csvfile:
     voidreader = csv.reader(csvfile, delimiter=",")
+    metricsConf = formatMetricConfiguration(loadMetricConfiguration())
+    
     for row in voidreader:
         jsonRequest = []
         if not os.path.exists("/tmp/crawler/"):
@@ -45,14 +58,12 @@ with open('voidlist.csv', 'rb') as csvfile:
         datasetLocations = [ f for f in listdir("/tmp/crawler/") if isfile(join("/tmp/crawler/",f)) ]      
         datasetStr = "/tmp/crawler/"  
         datasetStr += ",/tmp/crawler/".join(datasetLocations);
-        metricsConfiguration="[{\"@id\": \"urn:a\",\"@type\": [\"http://purl.org/eis/vocab/lmi#MetricConfiguration\"],\"http://purl.org/eis/vocab/lmi#metric\": [{\"@value\": \"eu.diachron.qualitymetrics.intrinsic.accuracy.POBODefinitionUsage\"}]}]"""
-        payload = {'Dataset' : datasetStr, 'QualityReportRequired' : 'false', 'MetricsConfiguration' : metricsConfiguration, 'BaseUri' : baseURI }
-        #url = "http://localhost:8080/Luzzu/compute_quality?Dataset="+datasetStr+"&QualityReportRequired=false&MetricsConfiguration="+metricsConfiguration+"&BaseUri='"+baseURI+"'"
+        print(metricsConf)
+        payload = {'Dataset' : datasetStr, 'QualityReportRequired' : 'false', 'MetricsConfiguration' : metricsConf, 'BaseUri' : baseURI }
         url="http://localhost:8080/Luzzu/compute_quality"
         r = requests.post(url, data=payload)
-        #r = requests.post(url)
         r.text
-        os.remove("/tmp/crawler/")
+        #os.remove("/tmp/crawler/")
     print('finished')
             
             
