b5be83967574ebcb05732e3357303392c197ae86
Santiago Londoño <londono@informatik.uni-bonn.de>
2015-04-16 00:07:50 +0200

    Updated crawler implementation.

--- crawler.py
+++ crawler.py
@@ -12,6 +12,7 @@ import sys
 
 from os import listdir
 from os.path import isfile, join
+from shutil import rmtree
 from rdflib.serializer import Serializer
 from rdflib import plugin
 
@@ -20,9 +21,16 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(relativeCr
 logger_crawl = logging.getLogger("crawler")
 logger_crawl.setLevel(logging.DEBUG)
 
-proxies = {
+# Configuration parameters
+proxies_default = {
   # From uni-bonn servers use: "http": "http://webcache.iai.uni-bonn.de:3128"
 }
+
+proxies_qty_srv = {
+  # No proxies required as the quality service is on a local URL
+}
+
+url_qty_srv = "http://localhost:8080/Luzzu/compute_quality"
 
 # Functions
 def identifySerialisation( fileName ):
@@ -50,7 +58,8 @@ def download(filename, folder):
     
 
 # Main
-proxy = urllib2.ProxyHandler(proxies)
+crawler_dir = "/srv/270a-crawling/results/"
+proxy = urllib2.ProxyHandler(proxies_default)
 opener = urllib2.build_opener(proxy)
 urllib2.install_opener(opener)
 
@@ -62,13 +71,13 @@ with open('voidlist.csv', 'rb') as csvfile:
     	logger_crawl.debug("Reading resource: {0}, void: {1}".format(row[1], row[0]))
     	
         jsonRequest = []
-        if not os.path.exists("/tmp/crawler/"):
-            os.makedirs("/tmp/crawler/")
+        if not os.path.exists(crawler_dir):
+            os.makedirs(crawler_dir)
             
         g = rdflib.Graph()
         baseURI = row[1]
         
-        folder = "/tmp/crawler/"+row[1].replace('http://','').replace('/','')
+        folder = join(crawler_dir, baseURI.replace('http://','').replace('/',''))
         if not os.path.exists(folder):
             os.makedirs(folder)
         folder += '/'
@@ -78,34 +87,38 @@ with open('voidlist.csv', 'rb') as csvfile:
         except rdflib.plugin.PluginException:
             result = g.parse(row[0], format=identifySerialisation(row[0]))
             
-        for row in g.query("""SELECT ?dataset WHERE 
-             { ?a <http://rdfs.org/ns/void#dataDump> ?dataset . }"""):           
+        datasetLocations = set([])
+            
+        for row in g.query("""SELECT ?a ?dataset WHERE 
+             { ?a <http://rdfs.org/ns/void#subset> ?dataset . ?a <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rdfs.org/ns/void#Dataset> . }"""): 
+             	datasetLocations.add("%s"%(row["a"]))
+                datasetLocations.add("%s"%(row["dataset"]))
              
-             filename = "%s"%(row)
-             logger_crawl.info("Downloading resource: {0}. Into folder: {1}".format(filename, folder))
-             download(filename, folder)
+#             filename = "%s"%(row)
+#             logger_crawl.info("Downloading resource: {0}. Into folder: {1}".format(filename, folder))
+#             download(filename, folder)
              
-             if (ntpath.basename(filename).endswith(".tar.gz")):
-             	logger_crawl.info("Extracting tar: {0} on directory: {1}".format(ntpath.basename(filename), folder))
-                tar = tarfile.open(folder+ntpath.basename(filename))
-                tar.extractall(folder)
-                tar.close()
-                os.remove(folder+ntpath.basename(filename))
+#             if (ntpath.basename(filename).endswith(".tar.gz")):
+#             	logger_crawl.info("Extracting tar: {0} on directory: {1}".format(ntpath.basename(filename), folder))
+#                tar = tarfile.open(folder+ntpath.basename(filename))
+#                tar.extractall(folder)
+#                tar.close()
+#                os.remove(folder+ntpath.basename(filename))
 
-        datasetLocations = [ "file:///"+join(folder,f) for f in listdir(folder) if isfile(join(folder,f)) ]      
+        # datasetLocations = [ "file:///"+join(folder,f) for f in listdir(folder) if isfile(join(folder,f)) ]      
         datasetStr = ",".join(datasetLocations);
         logger_crawl.info("Metrics config: {0}".format(metricsConf))
         
-        payload = {'Dataset' : datasetStr, 'QualityReportRequired' : 'false', 'MetricsConfiguration' : metricsConf, 'BaseUri' : baseURI }
-        url="http://localhost:8080/Luzzu/compute_quality"
-        logger_crawl.debug("Sending POST. URL: {0}. Dataset: {1}. Base URI: {2}".format(url, datasetStr, baseURI))
+        payload = {'Dataset' : datasetStr, 'QualityReportRequired' : 'true', 'MetricsConfiguration' : metricsConf, 'BaseUri' : baseURI }
+        logger_crawl.debug("Sending POST. URL: {0}. Dataset: {1}. Base URI: {2}".format(url_qty_srv, datasetStr, baseURI))
         
         try:
-            r = requests.post(url, data=payload, proxies=proxies)
-            logger_crawl.info("Quality assessment completed for: {0}. Result: {1}", row[1], r.text)
+            r = requests.post(url_qty_srv, data=payload, proxies=proxies_qty_srv)
+            logger_crawl.info("Quality assessment completed for: {0}. Result: {1}".format(row[1], r.text))
         except Exception as ex:
             logger_crawl.exception("Error processing request. Crawling aborted.")
             break
-        
+
     logger_crawl.info("Crawling finished")
-    os.remove("/tmp/crawler/")    
+    # Remove directory corresponding to this crawling process
+    # rmtree(crawler_dir) 
